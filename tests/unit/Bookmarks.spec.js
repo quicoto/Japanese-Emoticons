665a2ba75024acbd8c0fa75f49f2debef1202df7
Ricard Torres <torres.rick@gmail.com>
2018-08-06 13:45:36 +0200

    Fix tests

---
+++ tests/unit/Bookmarks.spec.js
@@ -0,0 +1,11 @@
+import { shallowMount } from '@vue/test-utils';
+import Bookmarks from '@/components/Bookmarks.vue';
+
+describe('Bookmarks.vue', () => {
+  it('Does nothing', () => {
+    const result = true;
+    const expected = true;
+
+    expect(result).toBe(expected);
+  });
+});
